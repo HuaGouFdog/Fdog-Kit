daab54c7027504e863257702e717ac25eb439ac8
HuaGouFdog <2506897252@qq.com>
2025-02-22 20:07:08 +0800

    新增数据库连接选项

--- Fdog-Kit/module_connect/createconnect.cpp
+++ Fdog-Kit/module_connect/createconnect.cpp
@@ -6,13 +6,16 @@
 #include <QDateTime>
 #include <QDebug>
 #include <QTextCodec>
+#include <QListView>
 createconnect::createconnect(int8_t connectType, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::createconnect)
 {
     ui->setupUi(this);
-    //ui->label->hide();
-    db = new sshsql();
+    ui->widget->hide();
+    ui->widget_dynamic_title->hide();
+    ui->comboBox->setView(new QListView());
+
     //只显示关闭按钮
     setWindowFlags(Qt::WindowCloseButtonHint);
     //选择显示的连接类型
@@ -24,20 +27,23 @@ createconnect::createconnect(int8_t connectType, QWidget *parent) :
     } else if (connectType == ZK_CONNECT_TYPE) {
         ui->tabWidget->setCurrentIndex(connectType - 1);
         setWindowTitle("新建zookeeper连接");
+    } else if (connectType == DB_CONNECT_TYPE) {
+        ui->tabWidget->setCurrentIndex(connectType - 1);
+        setWindowTitle("新建database连接");
     } else if (connectType == REDIS_CONNECT_TYPE) {
         ui->tabWidget->setCurrentIndex(connectType - 1);
         setWindowTitle("新建redis连接");
     } else if (connectType == KAFKA_CONNECT_TYPE) {
         ui->tabWidget->setCurrentIndex(connectType - 1);
         setWindowTitle("新建kafka连接");
-    } else if (connectType == DB_CONNECT_TYPE) {
-        ui->tabWidget->setCurrentIndex(connectType - 1);
-        setWindowTitle("新建database连接");
     }
 }
 
 createconnect::~createconnect()
 {
+    if (db != nullptr) {
+        delete db;
+    }
     delete ui;
 }
 
@@ -52,6 +58,9 @@ void createconnect::on_widget_bottom_toolButton_connect_clicked()
     //创建选择的连接信息
     connnectInfoStruct cInfo;
     if (ui->tabWidget->currentIndex() == 0) {
+        if (db == nullptr) {
+            db = new sshsql();
+        }
         cInfo.connectType = 1;//this->connectType;
         cInfo.name = ui->widget_name_lineEdit_name_data->text();
         if (ui->widget_name_lineEdit_name_data->text() == "") {
@@ -103,19 +112,26 @@ void createconnect::on_widget_bottom_toolButton_connect_clicked()
         }
         cInfo.host = ui->lineEdit_host_zk_data->text();
         cInfo.port = ui->lineEdit_port_zk_data->text();
-    }
+    } else if (ui->tabWidget->currentIndex() == 3) {
+        cInfo.connectType = 4;
+        cInfo.name = ui->widget_name_lineEdit_name_data->text();
+        if (ui->widget_name_lineEdit_name_data->text() == "") {
+            cInfo.name = ui->widget_name_lineEdit_name_data->placeholderText();
+        }
+        cInfo.group = ui->widget_group_lineEdit_group_data->text();
+        if (ui->widget_group_lineEdit_group_data->text() == "") {
+            cInfo.group = ui->widget_group_lineEdit_group_data->placeholderText();
+        }
+        cInfo.host = ui->lineEdit_host_db_data->text();
+        cInfo.port = ui->lineEdit_port_db_data->text();
 
-//    QHBoxLayout* layoutButton = new QHBoxLayout(ui->widget_bottom_toolButton_connect);
-//    layoutButton->addStretch(1);
-//    layoutButton->setContentsMargins(0,0,0,0);
-//    QLabel* label = new QLabel();
-//    label->setAlignment(Qt::AlignCenter);
-//    QMovie* movie = new QMovie(":/lib/jiazai5.gif");
-//    movie->setObjectName("movieLoading");
-//    movie->setScaledSize(QSize(20, 20));
-//    ui->label->show();
-//    ui->label->setMovie(movie);
-//    movie->start();
+        cInfo.userName = ui->lineEdit_user_db_data->text();
+        if (ui->lineEdit_user_db_data->text() == "") {
+            cInfo.userName = ui->lineEdit_user_db_data->placeholderText();
+        }
+        cInfo.password = ui->tab_passowrd_lineEdit_password_db_data->text();
+        cInfo.isSavePassword = ui->tab_passowrd_checkBox_remember_password_db->isChecked();
+    }
 
     emit newCreate(cInfo);
     this->hide();
